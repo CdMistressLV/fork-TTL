3010749b680142599dbaa6aa2150f26a89b93afb
CdMistressLV <mansnnothot482@gmail.com>
2026-09-15 22:35:24 +0100

    Finished the first design of the tts module, awaiting the implimentation of this into the main script

--- CML/tinkering/tts.py
+++ CML/tinkering/tts.py
@@ -1,6 +1,7 @@
 import wave
-from piper-tts import PiperVoice
+from piper import PiperVoice
 import tomllib as tr
+
 
 with open("config.toml", "r", encoding="utf-8") as f:
     config = tr.load(f)
@@ -10,11 +11,17 @@ platform = config["general"]["platform"]
 if platform == "Windows":
     import winsound as ws
 elif platform == "Linux":
-    import pygame as pg
+    import sounddevice as sd
+    import soundfile as sf
 
 def comment_speak(text: str, platform: str = platform):
     voice = PiperVoice.load(r"E:\Downloads\en_GB-cori-medium.onnx", r"E:\Downloads\en_GB-cori-medium.onnx.json")
     with wave.open("piper.wav", "wb") as wav_file:
         voice.synthesize_wav(text, wav_file)
 
-    ws.PlaySound("piper.wav", ws.SND_FILENAME)
+    if platform == "Windows":
+        ws.PlaySound("piper.wav", ws.SND_FILENAME)
+    elif platform == "Linux":
+        data, samplerate = sf.read("piper.wav")
+        sd.play(data, samplerate)
+        sd.wait()
